1c064b69fb93a03b1d563444d22f1672677c11fd
Maria Gruzdeva <maria.gruzdeva@russianpost.ru>
2023-01-04 16:34:51 +0300

    task_3

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,15 +14,16 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-    // 1. Создайте переменную itemCountText, равную пустой строке
-    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    let itemCountText = '' + product.count + ` × ` + product.price + ` ₽ = `;;  // 1.Создайте переменную itemCountText, равную пустой строке
+    console.log(itemCountText); // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    console.log(sum);
+    itemCountText = itemCountText + sum + ` ₽`; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
-
+    console.log(itemCountText);
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
